f05ab14d408acd7fd5c18c1495398595d4b2d018
Will Sidley-Parker <supacow101@gmail.com>
2022-03-28 14:59:10 -0500

    Initial Deployment branch working

--- src/main/java/com/cognixia/jump/JumpJanRainmanCapstoneApplication.java
+++ src/main/java/com/cognixia/jump/JumpJanRainmanCapstoneApplication.java
@@ -13,14 +13,6 @@ public class JumpJanRainmanCapstoneApplication {
 		SpringApplication.run(JumpJanRainmanCapstoneApplication.class, args);
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**").allowedOrigins("http://localhost:3000/**");
-			}
-		};
-	}
+	
 
 }
